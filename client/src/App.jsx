646503cec13aa2b17eb6d9e91770c9e6754bc7d6
Al-Abir <sheikhabir196@gmail.com>
2025-04-20 18:25:37 +0600

    Banner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div>
       {isSellerPath? null: <Navbar></Navbar> }
-      <div className={`${isSellerPath}? " ": "px-6 md:px-16 lg:px-24"`}>
+      <div className={`${isSellerPath?"":"px-6 md:px-16 lg:px-24"}`}>
          <Routes>
              <Route path='/'element={<Home></Home>}>
 
---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Categories = () => {
+  return (
+    <div>
+        <p>Categoris</p>
+        <div>
+            <img src={assets.box_icon} alt="" />
+            <p>fruit</p>
+        </div>
+    </div>
+  )
+}
+
+export default Categories
