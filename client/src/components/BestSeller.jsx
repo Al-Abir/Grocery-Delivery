208e438902e940796bbe80cb9b8e57bb42fbd7f8
Al-Abir <sheikhabir196@gmail.com>
2025-05-11 16:11:09 +0600

    All eroor fix done

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -5,19 +5,18 @@ import { useAppContext } from '../context/AppContext'
 const BestSeller = () => {
     const {products} = useAppContext()
   return (
-    <div className='mt-16'>
-        <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
-        <div className='mt-6 mb-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 l'>
-        {
-            products.filter((product,i)=> product.inStock).slice(0,5).map((product, index)=>(
+    <div className="mt-16 px-4 sm:px-6 md:px-8">
+  <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
 
-                <ProductCard product={product} key={index} ></ProductCard>
-            ))
-        }
+  <div className="mt-6 mb-5 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+    {
+      products.filter((product) => product.inStock).slice(0, 5).map((product, index) => (
+        <ProductCard product={product} key={index} />
+      ))
+    }
+  </div>
+</div>
 
-        </div>
-      
-    </div>
   )
 }
 
