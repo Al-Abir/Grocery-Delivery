d6cb0e47d6da23f3c93f5a478c03857be6fec701
Al-Abir <sheikhabir196@gmail.com>
2025-05-12 23:02:54 +0600

    deploy

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ await connectDB()
 await connectCloudinary()
 
 //allowMultiple orign
-const allowedOrgin = ["http://localhost:5173","https://grocery-delivery-five.vercel.app"]
+const allowedOrgin = ["http://localhost:5173","https://grocery-delivery-68rd.vercel.app"]
 
 //middleware configuration
 app.use(express.json());
